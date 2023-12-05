9b85bfecf6566a7020df36673776c3b68c730b85
Saidgo26 <saidgonzalezmo@gmail.com>
2023-12-04 23:15:33 -0400

    I created and uploaded my portfolio

---
+++ app.js
@@ -0,0 +1,26 @@
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+};
+
+
+
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+};
